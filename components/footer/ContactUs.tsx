871829f244ce6a2bff68b35b12cb99c6d6436728
Levon Stepanyan <levonstepanyan.ait@gmail.com>
2023-12-01 23:53:11 +0400

    update contact us info

--- components/footer/ContactUs.tsx
+++ components/footer/ContactUs.tsx
@@ -6,23 +6,38 @@ import facebook from "@images/facebook.svg";
 import email from "@images/email.svg";
 import geo from "@images/geo.svg";
 import address from "@images/address.svg";
+import instagram from "@images/instagram.svg";
 
 // Components
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Link } from "@mui/material";
 import Image from "next/image";
+
+const phoneInfo = [
+  {
+    text: `+374 (10) - 66 - 22 - 02`,
+    href: "tel:+37410662202"
+  },
+  {
+    text: `+374 (010) - 66 - 18 - 09`,
+    href: "tel:+37410661800"
+  }
+];
 
 const footer_items = [
   {
-    icon: phone,
-    text: `+374 (10) - 66 - 22 - 02`
+    icon: facebook,
+    text: `ՀՀ ՊՆ Վ.Սարգսյանի անվան ռազմական ակադեմիա`,
+    href: "https://www.facebook.com/vsargsyanma"
   },
   {
-    icon: facebook,
-    text: `ՀՀ ՊՆ Վ.Սարգսյանի անվան ռազմական ակադեմիա`
+    icon: instagram,
+    text: "ՀՀ ՊՆ Վ.Սարգսյանի անվան ռազմական ակադեմիա",
+    href: "https://www.instagram.com/vsargsyan_ma/"
   },
   {
     icon: email,
-    text: `mu.vs@mil.am`
+    text: `mu.vs@mil.am`,
+    href: "mailto:mu.vs@mil.am"
   },
   {
     icon: geo,
@@ -37,16 +52,39 @@ const footer_items = [
 export default function ContactUs() {
   return (
     <Grid container rowSpacing={{ xs: 3, sm: 5 }} py="4vh">
-      {footer_items.map((el, i) => (
-        <Grid item xs={12} key={el.text + i}>
-          <Grid container alignItems="center" columnGap={{ xs: 2, sm: 4 }}>
-            <Grid item xs={1}>
-              <Image width={30} height={30} src={el.icon} alt="contactIcons" />
-            </Grid>
-            <Grid item xs={10}>
-              <Typography variant="h3">{el.text}</Typography>
+      <Grid item xs={12}>
+        <Grid container alignItems="center" columnGap={{ xs: 2, sm: 4 }}>
+          <Grid item xs={1}>
+            <Image width={30} height={30} src={phone} alt="contactIcons" />
+          </Grid>
+          <Grid item xs={10}>
+            <Grid container>
+              <Link href={phoneInfo[0].href}>
+                <Typography variant="h3">
+                  {phoneInfo[0].text}
+                  &nbsp;,&nbsp;
+                </Typography>
+              </Link>
+              <Link href={phoneInfo[1].href}>
+                <Typography variant="h3">{phoneInfo[1].text}</Typography>
+              </Link>
             </Grid>
           </Grid>
+        </Grid>
+      </Grid>
+
+      {footer_items.map((el, i) => (
+        <Grid item xs={12} key={el.text + i}>
+          <Link href={el.href}>
+            <Grid container alignItems="center" columnGap={{ xs: 2, sm: 4 }}>
+              <Grid item xs={1}>
+                <Image width={30} height={30} src={el.icon} alt="contactIcons" />
+              </Grid>
+              <Grid item xs={10}>
+                <Typography variant="h3">{el.text}</Typography>
+              </Grid>
+            </Grid>
+          </Link>
         </Grid>
       ))}
     </Grid>
